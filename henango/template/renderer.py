5278aa2828e26ea327fdc52c5f1f31b59f4ff65f
保苅庸平 <hokariyouhei@hokariyouheinoMacBook-Pro.local>
2024-11-23 08:32:28 +0900

    first commit

---
+++ henango/template/renderer.py
@@ -0,0 +1,11 @@
+import os
+
+import settings
+
+def render(template_name: str, context: dict):
+    template_path = os.path.join(settings.TEMPLATES_DIR, template_name)
+
+    with open(template_path) as f:
+        template = f.read()
+
+    return template.format(**context)
